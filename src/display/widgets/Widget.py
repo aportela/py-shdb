7884863b80e6de92ece70785c4ff0a3e2b0bf640
alex <766f6964+github@gmail.com>
2024-12-15 12:07:48 +0100

    rewrited base widget class

--- src/display/widgets/Widget.py
+++ src/display/widgets/Widget.py
@@ -1,12 +1,42 @@
-class Widget:
-    def __init__(self):
-        self._width = 0
-        self._height = 0
-        self._xOffset = 0
-        self._yOffset = 0
-        self._padding = 0
-        self._xOffsetWithPadding = 0
-        self._yOffsetWithPadding = 0
+import pygame
+from abc import ABC, abstractmethod
 
+class Widget(ABC):
+    def __init__(self, surface: pygame.Surface, debug: bool = False, x_offset: int = 0, y_offset: int = 0, width: int = 0, height: int = 0, padding: int = 1):
+        if width < 0 or height < 0 or padding < 0:
+            raise ValueError("Invalid width/height/padding")
+
+        self._debug = debug
+        self._surface = surface
+        self._x_offset = x_offset
+        self._y_offset = y_offset
+        self._width = width
+        self._height = height
+        self._padding = padding
+        self._changed = False
+
+        # temporal surface for this widget
+        self._tmp_surface = pygame.Surface((self._width, self._height))
+        self._tmp_surface.fill((0, 0, 0))
+
+    # clear temporal widget surface
+    def clear(self):
+        self._tmp_surface.fill((0, 0, 0))
+
+    # dump temporal widget surface on main surface
+    def blit(self):
+        if self._debug:
+            # add border (for debug sizes/offsets)
+            pygame.draw.rect(self._tmp_surface, (255, 100, 50), (0, 0, self._width , self._height), 1)
+        # dump surface
+        self._surface.blit(self._tmp_surface, (self._x_offset, self._y_offset))
+
+    @abstractmethod
     def refresh(self, force: bool = False) -> bool:
-        return True
+        """
+        abstract method for refreshing widget
+
+        :param force: if true, always refresh (warning, high cpu cost)
+        :return: returns true if widget detect changes
+        """
+        pass
