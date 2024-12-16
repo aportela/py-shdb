38a8f764564f4ae11d197bf6a168c179e8b54d10
alex <766f6964+github@gmail.com>
2024-12-16 18:18:22 +0100

    added bold font & changed color

--- src/display/widgets/rss_widget.py
+++ src/display/widgets/rss_widget.py
@@ -8,7 +8,7 @@ class RSSWidget(Widget):
     def __init__(self, surface: pygame.Surface, debug: bool = False, x_offset: int = 0, y_offset: int = 0, width: int = 0, height: int = 0, padding: int = 0, font_family: str = "monospace", font_size: int = 12, url: str = "", default_seconds_refresh_time: int = 600, max_items: int = 16):
         # Llamada al constructor de la clase base
         super().__init__(surface=surface, debug=debug, x_offset=x_offset, y_offset=y_offset, width=width, height=height, padding=padding)
-        self._font = pygame.font.SysFont(font_family, font_size)
+        self._font = pygame.font.SysFont(font_family, font_size, bold = True)
         self._module = RSSFeed(url, default_seconds_refresh_time, max_items)
         # TODO: try / except
         self._module.get(True)
@@ -20,12 +20,12 @@ class RSSWidget(Widget):
 
         if (force or data["changed"]):
             self.clear()
-            title_text = self._font.render(data["title"], True, (255, 255, 0))
+            title_text = self._font.render(data["title"], True, (255, 255, 255))
             y = self._padding
             self._tmp_surface.blit(title_text, (self._padding, y))
             y += title_text.get_height() + 4
             for item in data["items"]:
-                item_text = self._font.render(item["title"], True, (255, 255, 0))
+                item_text = self._font.render(item["title"], True, (255, 255, 255))
                 self._tmp_surface.blit(item_text, (self._padding, y))
                 y += title_text.get_height() + 4
             super().blit()
