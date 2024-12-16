8bf403af3c32e3280d0cf9e8222fcfd39e1bbe9e
alex <766f6964+github@gmail.com>
2024-12-16 22:17:22 +0100

    new reload method, changed (debug) border color

--- src/display/widgets/Widget.py
+++ src/display/widgets/Widget.py
@@ -1,6 +1,8 @@
 import pygame
 from abc import ABC, abstractmethod
 
+WIDGET_BORDER_COLOR=(255, 105, 180)
+
 class Widget(ABC):
     def __init__(self, surface: pygame.Surface, debug: bool = False, x: int = 0, y: int = 0, width: int = 0, height: int = 0, padding: int = 1):
         if width < 0 or height < 0 or padding < 0:
@@ -19,6 +21,14 @@ class Widget(ABC):
         self._tmp_surface = pygame.Surface((self._width, self._height))
         self._tmp_surface.fill((0, 0, 0))
 
+    # reload widget configuration
+    def reload(self, x: int = 0, y: int = 0, width: int = 0, height: int = 0, padding: int = 1):
+        self._x = x
+        self._y = y
+        self._width = width
+        self._height = height
+        self._padding = padding
+
     # clear temporal widget surface
     def clear(self):
         self._tmp_surface.fill((0, 0, 0))
@@ -27,7 +37,7 @@ class Widget(ABC):
     def blit(self):
         if self._debug:
             # add border (for debug sizes/offsets)
-            pygame.draw.rect(self._tmp_surface, (255, 100, 50), (0, 0, self._width , self._height), 1)
+            pygame.draw.rect(self._tmp_surface, WIDGET_BORDER_COLOR, (0, 0, self._width , self._height), 1)
         # dump surface
         self._surface.blit(self._tmp_surface, (self._x, self._y))
 
