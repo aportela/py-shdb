8a855117c79b9ad4231b405f6530bd6dec0dc534
alex <766f6964+github@gmail.com>
2024-12-18 00:58:42 +0100

    params updated & use new widget blit method

--- src/display/widgets/simple_label_widget.py
+++ src/display/widgets/simple_label_widget.py
@@ -5,9 +5,18 @@ from .widget_font import WidgetFont
 
 class SimpleLabelWidget(Widget):
 
-    def __init__(self, name: str, x: int , y: int, width: int, height: int, padding: int, surface: pygame.Surface, debug: bool, font: WidgetFont, text: str):
-        super().__init__(name=name, surface=surface, debug=debug, x=x, y=y, width=width, height=height, padding=padding)
-        self._font = font
+    def __init__(self, name: str, x: int , y: int, width: int, height: int, padding: int, border: bool = False, surface: pygame.Surface = None, font: WidgetFont = None, text: str = None):
+        # Initialize the parent class (Widget) with the provided parameters
+        super().__init__(name = name, x = x, y = y, width = width, height = height, padding = padding, border = border, surface = surface)
+                # Ensure that the font is provided, otherwise raise an error
+        if font is None:
+            raise RuntimeError("Font not set")  # Font must be provided
+        else:
+            self.__font = font  # Set the font for rendering text
+
+        # Ensure that the text is provided, otherwise raise an error
+        if text is None:
+            raise RuntimeError("Text not set")  # Text must be provided
         self._text = text
         self._render_required = True # this widget has static text (no changes) so only render on first refresh iteration
 
@@ -15,10 +24,7 @@ class SimpleLabelWidget(Widget):
         if force or self._render_required:
             self._render_required = False
             self._clear()
-            self._tmp_surface.blit(
-                self._font.render(self._text),
-                (self._padding, self._padding)
-            )
+            self._blit(self.__font.render(self._text))
             super()._render()
             return True
         else:
