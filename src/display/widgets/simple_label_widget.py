c2b5176ad7ce0e0a2928fe08508d9350d550135d
alex <766f6964+github@gmail.com>
2024-12-18 00:59:04 +0100

    documentation added

--- src/display/widgets/simple_label_widget.py
+++ src/display/widgets/simple_label_widget.py
@@ -5,10 +5,11 @@ from .widget_font import WidgetFont
 
 class SimpleLabelWidget(Widget):
 
-    def __init__(self, name: str, x: int , y: int, width: int, height: int, padding: int, border: bool = False, surface: pygame.Surface = None, font: WidgetFont = None, text: str = None):
+    def __init__(self, name: str, x: int, y: int, width: int, height: int, padding: int, border: bool = False, surface: pygame.Surface = None, font: WidgetFont = None, text: str = None):
         # Initialize the parent class (Widget) with the provided parameters
-        super().__init__(name = name, x = x, y = y, width = width, height = height, padding = padding, border = border, surface = surface)
-                # Ensure that the font is provided, otherwise raise an error
+        super().__init__(name=name, x=x, y=y, width=width, height=height, padding=padding, border=border, surface=surface)
+
+        # Ensure that the font is provided, otherwise raise an error
         if font is None:
             raise RuntimeError("Font not set")  # Font must be provided
         else:
@@ -18,14 +19,26 @@ class SimpleLabelWidget(Widget):
         if text is None:
             raise RuntimeError("Text not set")  # Text must be provided
         self._text = text
-        self._render_required = True # this widget has static text (no changes) so only render on first refresh iteration
+
+        # Initialize the render flag. This widget has static text (no changes) so only render on the first refresh iteration
+        self._render_required = True
 
     def refresh(self, force: bool = False) -> bool:
+        """
+        Refreshes the widget by rendering the text if necessary.
+        If the 'force' argument is True or if a render is required,
+        the widget will be rendered.
+        Otherwise, it won't refresh.
+        """
         if force or self._render_required:
-            self._render_required = False
-            self._clear()
+            self._render_required = False  # Set the render flag to False since we are rendering the widget
+            self._clear()  # Clear the previous content
+
+            # Render the text using the specified font and blit it to the surface
             self._blit(self.__font.render(self._text))
+
+            # Call the parent class to handle additional rendering logic
             super()._render()
-            return True
+            return True  # Indicate that the widget was rendered successfully
         else:
-            return False
+            return False  # Return False if the widget doesn't need a refresh
