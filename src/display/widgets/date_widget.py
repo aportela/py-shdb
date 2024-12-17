879db2acb336056116277d0d8ee04c7c3bab16f1
alex <766f6964+github@gmail.com>
2024-12-18 00:41:35 +0100

    params update & use new widget blit method

--- src/display/widgets/date_widget.py
+++ src/display/widgets/date_widget.py
@@ -6,22 +6,22 @@ from .widget_font import WidgetFont
 
 class DateWidget(Widget):
 
-    def __init__(self, name: str, x: int , y: int, width: int, height: int, padding: int, surface: pygame.Surface, debug: bool, font: WidgetFont, format_mask: str = "%A, %B %d"):
-        super().__init__(name=name, surface=surface, debug=debug, x=x, y=y, width=width, height=height, padding=padding)
-        self._font = font
-        self._format_mask = format_mask
-        self._str = None
+    def __init__(self, name: str, x: int , y: int, width: int, height: int, padding: int, border: bool = False, surface: pygame.Surface = None, font: WidgetFont = None, format_mask: str = "%A, %B %d"):
+        # Initialize the parent class (Widget)
+        super().__init__(name = name, x = x, y = y, width = width, height = height, padding = padding, border = border, surface = surface)
+        if font == None:
+            raise RuntimeError("font not set")
+        else:
+            self.__font = font
+        self.__format_mask = format_mask
+        self.__str = None
 
     def refresh(self, force: bool = False) -> bool:
-        now = datetime.datetime.now()
-        new_str = now.strftime(self._format_mask).title()
-        if (force or self._str != new_str):
-            self._str == new_str
+        new_str = datetime.datetime.now().strftime(self.__format_mask).title()
+        if (force or self.__str != new_str):
+            self.__str == new_str
             self._clear()
-            self._tmp_surface.blit(
-                self._font.render(new_str),
-                (self._padding, self._padding)
-            )
+            super()._blit(self.__font.render(new_str))
             super()._render()
             return True
         else:
