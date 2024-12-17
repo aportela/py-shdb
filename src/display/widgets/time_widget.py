af3e59e8dfa55bfcbd259e682d704c89d785c2c8
alex <766f6964+github@gmail.com>
2024-12-18 00:59:56 +0100

    params updated & use new widget blit method

--- src/display/widgets/time_widget.py
+++ src/display/widgets/time_widget.py
@@ -6,9 +6,14 @@ from .widget_font import WidgetFont
 
 class TimeWidget(Widget):
 
-    def __init__(self, name: str, x: int , y: int, width: int, height: int, padding: int, surface: pygame.Surface, debug: bool, font: WidgetFont, format_mask: str = "%I:%M %p"):
-        super().__init__(name=name, surface=surface, debug=debug, x=x, y=y, width=width, height=height, padding=padding)
-        self._font = font
+    def __init__(self, name: str, x: int , y: int, width: int, height: int, padding: int, border: bool = False, surface: pygame.Surface = None, font: WidgetFont = None, format_mask: str = "%I:%M %p"):
+        # Initialize the parent class (Widget) with the provided parameters
+        super().__init__(name = name, x = x, y = y, width = width, height = height, padding = padding, border = border, surface = surface)
+        # Ensure that the font is provided, otherwise raise an error
+        if font is None:
+            raise RuntimeError("Font not set")  # Font must be provided
+        else:
+            self.__font = font  # Set the font for rendering text
         self._format_mask = format_mask
         self._str = None
 
@@ -19,10 +24,7 @@ class TimeWidget(Widget):
         if (force or self._str != new_str):
             self._str == new_str
             self._clear()
-            self._tmp_surface.blit(
-                self._font.render(new_str),
-                (self._padding, self._padding)
-            )
+            self._blit(self._font.render(new_str))
             super()._render()
             return True
         else:
