bd0d8b8d98810a4c2aef8df96df4ecaca207ca89
alex <766f6964+github@gmail.com>
2024-12-19 18:03:14 +0100

    removed enum separators, added direction param

--- src/display/widgets/widget_font_awesome.py
+++ src/display/widgets/widget_font_awesome.py
@@ -9,33 +9,37 @@ class FontAwesomeIcon(Enum):
 
 # https://docs.fontawesome.com/web/style/animate#_top
 class Effect(Enum):
-    NONE = 0,
-    BEAT = 1,
-    FADE = 2,
-    BEAT_AND_FADE = 3,
-    BOUNCE = 4,
-    HORIZONTAL_FLIP = 5,
-    VERTICAL_FLIP = 6,
-    SHAKE = 7,
+    NONE = 0
+    BEAT = 1
+    FADE = 2
+    BEAT_AND_FADE = 3
+    BOUNCE = 4
+    HORIZONTAL_FLIP = 5
+    VERTICAL_FLIP = 6
+    SHAKE = 7
     SPIN = 8
     SPIN_REVERSE = 9
 # https://docs.fontawesome.com/web/style/animate#_top
 class FontAwesomeEffect(Enum):
-    NONE = 0,
-    BEAT = 1,
-    FADE = 2,
-    BEAT_AND_FADE = 3,
-    BOUNCE = 4,
-    HORIZONTAL_FLIP = 5,
-    VERTICAL_FLIP = 6,
-    SHAKE = 7,
+    NONE = 0
+    BEAT = 1
+    FADE = 2
+    BEAT_AND_FADE = 3
+    BOUNCE = 4
+    HORIZONTAL_FLIP = 5
+    VERTICAL_FLIP = 6
+    SHAKE = 7
     SPIN = 8
     SPIN_REVERSE = 9
 
 class FontAwesomeEffectSpeed(Enum):
-    SLOW = 0,
-    MEDIUM = 1,
-    FAST = 2
+    SLOW = 1
+    MEDIUM = 2
+    FAST = 4
+
+class FontAwesomeSpinEffectDirection(Enum):
+    NORMAL = 1,
+    REVERSED = 2
 
 
 class FontAwesomeBaseEffect(WidgetFont):
@@ -49,19 +53,22 @@ class FontAwesomeBaseEffect(WidgetFont):
         pass
 
 class FontAwesomeSpinEffect(FontAwesomeBaseEffect):
-    def __init__(self, file: str = None, size: int = 30, color: tuple = (255, 255, 255), speed: FontAwesomeEffectSpeed = FontAwesomeEffectSpeed.MEDIUM) -> None:
+    def __init__(self, file: str = None, size: int = 30, color: tuple = (255, 255, 255), speed: FontAwesomeEffectSpeed = FontAwesomeEffectSpeed.MEDIUM, direction: FontAwesomeSpinEffectDirection = FontAwesomeSpinEffectDirection.NORMAL) -> None:
         super().__init__(file = file, size = size, color = color)
         self._effect = Effect.BEAT
-        self.__speed = 4
         self.__radius = 0
+        self.__speed = speed.value
         self.__angle = 0
+        self.__direction = direction
+        if (direction == FontAwesomeSpinEffectDirection.REVERSED):
+            self.__angle = 360
+
 
     def render_animation(self, icon: FontAwesomeIcon, custom_color: tuple = None) -> pygame.Surface:
-        radius = 0
         icon_surface = self.render(icon.value, custom_color)
         center = (icon_surface.get_width() / 2, icon_surface.get_height() / 2)
-        x = center[0] + radius * math.cos(math.radians(self.__angle))
-        y = center[1] + radius * math.sin(math.radians(self.__angle))
+        x = center[0] + self.__radius * math.cos(math.radians(self.__angle))
+        y = center[1] + self.__radius * math.sin(math.radians(self.__angle))
 
         rotated_icon = pygame.transform.rotate(icon_surface, self.__angle)
         rotated_rect = rotated_icon.get_rect(center=(x, y))
@@ -77,9 +84,14 @@ class FontAwesomeSpinEffect(FontAwesomeBaseEffect):
         # Render the text using the specified font and blit it to the surface
         square_surface.blit(rotated_icon, rotated_rect)
 
-        self.__angle = self.__angle + 4
-        if self.__angle > 360:
-            self.__angle = 0
+        if self.__direction == FontAwesomeSpinEffectDirection.NORMAL:
+            self.__angle = self.__angle + self.__speed
+            if self.__angle > 360:
+                self.__angle = 0
+        else:
+            self.__angle = self.__angle - self.__speed
+            if self.__angle >= 0:
+                self.__angle = 360
         return square_surface
 
 class WidgetFontAwesome(WidgetFont):
