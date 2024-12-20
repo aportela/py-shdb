64fdb57a5cc2501af5f821584eb0b9c00d966a8f
alex <766f6964+github@gmail.com>
2024-12-20 02:04:19 +0100

    added missing styles

--- src/display/widgets/widget_font.py
+++ src/display/widgets/widget_font.py
@@ -3,7 +3,6 @@ import pygame
 class WidgetFont:
     def __init__(self, family: str = None, file: str = None, size: int = 30, color: tuple = (255, 255, 255),
                  style_bold: bool = False, style_italic: bool = False):
-        """Initialize the font with the specified properties."""
         self.__family = family
         self.__file = file
         self.__size = size
@@ -13,21 +12,19 @@ class WidgetFont:
         self.__font = self.__initialize_font()
 
     def __initialize_font(self) -> pygame.font.Font:
-        """Initialize the font based on file, family, or default settings."""
         if self.__file:
             try:
-                return pygame.font.Font(self.__file, self.__size)
+                return pygame.font.Font(self.__file, size = self.__size)
             except Exception as e:
-                print(f"Error loading font file '{self.__file}': {e}")
-                return pygame.font.Font(None, self.__size)
+                print(f"Error loading font file '{self.__file}': {e}.")
+                return pygame.font.Font(name = None, size = self.__size, bold = self.__style_bold, italic = self.__style_italic)
         elif self.__family:
-            return pygame.font.SysFont(self.__family, self.__size, self.__style_bold, self.__style_italic)
+            return pygame.font.SysFont(name = self.__family, size = self.__size, bold = self.__style_bold, italic = self.__style_italic)
         else:
             return pygame.font.Font(None, self.__size)
 
     def update_font(self, family: str = None, file: str = None, size: int = None, color: tuple = None,
                     style_bold: bool = None, style_italic: bool = None) -> None:
-        """Update font properties and reinitialize the font."""
         if family is not None:
             self.__family = family
         if file is not None:
@@ -44,5 +41,4 @@ class WidgetFont:
         self.__font = self.__initialize_font()
 
     def render(self, text: str, custom_color: tuple = None) -> pygame.Surface:
-        """Render the specified text as a pygame.Surface."""
-        return self.__font.render(text, True, custom_color or self.__color)
+        return self.__font.render( text,  True, (custom_color if custom_color else self.__color))
