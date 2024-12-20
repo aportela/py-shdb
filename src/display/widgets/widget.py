45c252378341cdbe780d89b0b174155325844155
alex <766f6964+github@gmail.com>
2024-12-20 01:52:40 +0100

    added alpha (transparency) support

--- src/display/widgets/widget.py
+++ src/display/widgets/widget.py
@@ -2,63 +2,68 @@ import pygame
 from abc import ABC, abstractmethod
 from ...utils.logger import Logger
 
-WIDGET_BORDER_COLOR=(255, 105, 180)
+DEFAULT_WIDGET_BORDER_COLOR=(255, 105, 180) # PINK
 
 class Widget(ABC):
-    def __init__(self, name: str, x: int , y: int, width: int, height: int, padding: int, background_color: tuple[int, int, int, int] = (0, 0, 0, 0), border: bool = False, surface: pygame.Surface = None):
+    def __init__(self, surface: pygame.Surface, name: str, x: int, y: int, width: int, height: int, padding: int, background_color: tuple[int, int, int, int] = (0, 0, 0, 0), border: bool = False, border_color: tuple[int, int, int] = DEFAULT_WIDGET_BORDER_COLOR):
         self._log = Logger()
-        if width < 0 or height < 0 or padding < 0:
-            raise ValueError("Invalid width/height/padding")
+        self.__surface = surface
+        if not name:
+            raise ValueError("Name cannot be None or empty.")
         self.__name = name
+        self.__x =    x
+        self.__y = y
+        if width < 1 or height < 1 or padding < 0:
+            raise ValueError("Invalid width/height/padding.")
+        self.__width = width
+        self.__height = height
+        self.__padding = padding
         self.__background_color = background_color
         self.__border = border
-        self._surface = surface
-        self._x = x
-        self._y = y
-        self._width = width
-        self._height = height
-        self._padding = padding
+        self.__border_color = border_color
         self.___rect = pygame.Rect(x, y, width, height)
-
-        # temporal surface for this widget
-        self._tmp_surface = pygame.Surface((self._width, self._height))
-        self._tmp_surface.fill((0, 0, 0))
+        if len(background_color) == 4:
+            self._tmp_surface = pygame.Surface((self.__width, self.__height), pygame.SRCALPHA)
+        else:
+            self._tmp_surface = pygame.Surface((self.__width, self.__height))
 
     @property
     def name(self) -> str:
         return self.__name
 
-    # clear temporal widget surface
+    @property
+    def width(self) -> str:
+        return self.__width
+
+    @property
+    def height(self) -> str:
+        return self.__height
+
+    @property
+    def padding(self) -> str:
+        return self.__padding
+
     def _clear(self):
         self._tmp_surface.fill(self.__background_color)
 
     def _blit(self, surface: pygame.Surface, dest: tuple[int, int] = None):
         if dest is None:
-            dest = (self._padding, self._padding)
+            dest = (self.__padding, self.__padding)
         self._tmp_surface.blit(surface, dest)
 
-    # dump temporal widget surface on main surface
     def _render(self):
         if self.__border:
-            # add border (for debug sizes/offsets)
-            pygame.draw.rect(self._tmp_surface, WIDGET_BORDER_COLOR, (0, 0, self._width , self._height), 1)
-        # dump surface
-        self._surface.blit(self._tmp_surface, (self._x, self._y))
+            pygame.draw.rect(self._tmp_surface, self.__border_color, (0, 0, self.__width , self.__height), 1)
+        self.__surface.blit(self._tmp_surface, (self.__x, self.__y))
 
     @abstractmethod
     def refresh(self, force: bool = False) -> bool:
-        """
-        abstract method for refreshing widget
-
-        :param force: if true, always refresh (warning, high cpu cost)
-        :return: returns true if widget detect changes
-        """
         pass
 
     def verify_click(self, event):
         if self.___rect.collidepoint(event.pos):
             self.on_click()
 
-    @abstractmethod
     def on_click(self):
-        pass
+        self._log.debug("detected widget click event, but click event not found")
+        self.refresh(True)
