386ebdac3a441b21c0b65aafe98e128dadda9922
alex <766f6964+github@gmail.com>
2024-12-20 12:31:23 +0100

    allow change size & color

--- src/display/font_awesome_icon.py
+++ src/display/font_awesome_icon.py
@@ -6,8 +6,17 @@ class FontAwesomeIcon():
     def __init__(self, file: str, size: int, color: tuple = (255, 255, 255)) -> None:
         if not os.path.exists(file):
             raise ValueError(f"font awesome external file {file} not found.")
-        self.__font = pygame.font.Font(file, size)
+        self._font = pygame.font.Font(file, size)
+        self.__file = file
+        self.__size = size
+        self.__color = color
+
+    def set_size(self, size: int):
+        self.__size = size
+        self._font = pygame.font.Font(self.__file, self.__size)
+
+    def set_color(self, color: tuple):
         self.__color = color
 
     def render(self, icon: FontAwesomeUnicodeIcons, custom_color: tuple = None) -> pygame.Surface:
-        return self.__font.render(icon, True, (custom_color if custom_color else self.__color))
+        return self._font.render(icon, True, (custom_color if custom_color else self.__color))
