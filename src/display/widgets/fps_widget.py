947d06515691384c9626e2f5048f76e7ce0a4567
alex <766f6964+github@gmail.com>
2024-12-22 03:06:46 +0100

    new fps widget

---
+++ src/display/widgets/fps_widget.py
@@ -0,0 +1,29 @@
+import pygame
+
+from .widget import Widget, DEFAULT_WIDGET_BORDER_COLOR
+from .widget_font import WidgetFont
+from ..fps import FPS
+
+class FPSWidget(Widget):
+
+    def __init__(self, parent_surface: pygame.Surface, name: str, x: int , y: int, width: int, height: int, padding: int, background_color: tuple[int, int, int] = None, border: bool = False, border_color: tuple[int, int, int] = DEFAULT_WIDGET_BORDER_COLOR, font: WidgetFont = None) -> None:
+        super().__init__(parent_surface = parent_surface, name = name, x = x, y = y, width = width, height = height, padding = padding, background_color = background_color, border = border, border_color = border_color)
+        if not font:
+            raise RuntimeError("Font not set")
+        self.__font = font
+        self.__previousFPS = None
+
+    def refresh(self, force: bool = False) -> bool:
+        current_fps = FPS.get_current_fps()
+        if force or self.__previousFPS != current_fps:
+            self.__previousFPS = current_fps
+            super()._clear()
+            super()._blit(self.__font.render(f"FPS: {current_fps:03d}"))
+            super()._render()
+            return True
+        else:
+            return False
+
+    def on_click(self):
+        self._log.debug("Detected widget click event, forcing refresh")
+        self.refresh(True)
