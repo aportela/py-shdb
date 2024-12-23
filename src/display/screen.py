b77ead499bda959482838ade26630b3b11923d7f
alex <766f6964+github@gmail.com>
2024-12-23 15:47:26 +0100

    use configuration object

--- src/display/screen.py
+++ src/display/screen.py
@@ -1,16 +1,14 @@
 import pygame
+from ..utils.config import Configuration
 
 class Screen:
 
-    __screen_info = None
-    __screen_resolution = None
-    __main_surface = None
-
-    def __init__(self, app_name: str) -> None:
+    def __init__(self, configuration: Configuration) -> None:
         self.__screen_info = pygame.display.Info()
         self.__screen_resolution = (self.__screen_info.current_w, self.__screen_info.current_h)
         self.__main_surface = pygame.display.set_mode(size = self.__screen_resolution, flags = pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.NOFRAME)
-        pygame.display.set_caption(app_name)
+        pygame.display.set_caption(configuration.app_name)
+
     @property
     def width(self) -> int:
         return self.__screen_info.current_w
