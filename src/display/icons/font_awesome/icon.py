a687fb1aaa53544e560f4b925f51591677d266bb
alex <766f6964+github@gmail.com>
2024-12-24 01:57:27 +0100

    code cleanups

--- src/display/icons/font_awesome/icon.py
+++ src/display/icons/font_awesome/icon.py
@@ -1,41 +1,47 @@
 from typing import Optional
 import os
 import pygame
-from .icon_list import IconList as FontAwesomeIcon
 
+from .icon_list import IconList as FontAwesomeIcon
 from ....utils.logger import Logger
 
 class Icon():
 
-    __default_font_path = None
+    __default_font_path: Optional[str] = None
 
     def __init__(self, font_path: Optional[str] = None, size: int = 16, color: tuple[int, int, int] = (255, 255, 255)) -> None:
         if font_path is not None:
             if not os.path.exists(font_path):
                 raise ValueError(f"Font awesome external file path {font_path} not found.")
             else:
-                Icon.__default_font_path = font_path
+                self.__font_path = font_path
         elif Icon.__default_font_path is None:
             raise ValueError(f"Font awesome external file path not set.")
-        self.__font_path = font_path
         self.__size = size
         self.__color = color
-        self._font = pygame.font.Font(font_path, size)
+        self.__font = self._load_font()
 
     @staticmethod
     def set_default_font_path(font_path) -> None:
         if os.path.exists(font_path):
             Icon.__default_font_path = font_path
-            Logger().debug(f"Setting default FontAwesome path: {font_path}")
+            Logger().debug(f"Setting default FontAwesome font file path: {font_path}")
         else:
-            raise ValueError(f"Font awesome external file {font_path} not found.")
+            raise ValueError(f"Font awesome font file {font_path} not found.")
+
+    def _load_font(self) -> pygame.font.Font:
+        try:
+            return pygame.font.Font(self.__font_path, self.__size)
+        except Exception as e:
+            raise RuntimeError(f"Failed to load font at {self.__font_path}: {e}")
 
     def set_size(self, size: int) -> None:
         self.__size = size
-        self._font = pygame.font.Font(self.__font_path, self.__size)
+        self.__font = self._load_font()
 
     def set_color(self, color: tuple[int, int, int]) -> None:
         self.__color = color
 
     def render(self, icon: FontAwesomeIcon, custom_color: tuple[int, int, int] = None) -> pygame.Surface:
-        return self._font.render(icon, True, (custom_color if custom_color else self.__color))
+        color = custom_color if custom_color else self.__color
+        return self.__font.render(icon, True, color)
