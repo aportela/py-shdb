49f9905e386b66ddaad59081370a374e35ff39a8
alex <766f6964+github@gmail.com>
2024-12-24 10:41:49 +0100

    fade effect rewritted

--- src/display/icons/font_awesome/animations/fade.py
+++ src/display/icons/font_awesome/animations/fade.py
@@ -5,19 +5,19 @@ from ..icon_list import IconList as FontAwesomeIcons
 from ..enums import AnimationType as FontAwesomeAnimationType, AnimationSpeed as FontAwesomeAnimationSpeed
 
 class FontAwesomeIconFadeEffect(FontAwesomeIconBaseEffect):
-    def __init__(self, parent_surface: pygame.Surface, icon: FontAwesomeIcons, font_path: Optional[str] = None, size: int = 16, color: tuple[int, int, int] = (255, 255, 255), speed: FontAwesomeAnimationSpeed = FontAwesomeAnimationSpeed.MEDIUM, direction: FontAwesomeAnimationSpinDirection = FontAwesomeAnimationSpinDirection.CLOCKWISE) -> None:
-        super().__init__(parent_surface = parent_surface, icon = icon, font_path = font_path, size = size, color = color, background_color = background_color, speed = speed)
+    def __init__(self, parent_surface: pygame.Surface, icon: FontAwesomeIcons, font_path: Optional[str] = None, size: int = 16, color: tuple[int, int, int] = (255, 255, 255), speed: FontAwesomeAnimationSpeed = FontAwesomeAnimationSpeed.MEDIUM) -> None:
+        super().__init__(parent_surface = parent_surface, icon = icon, font_path = font_path, size = size, color = color, speed = speed)
         self._animation_type = FontAwesomeAnimationType.FADE
-        self.__current_alpha = 0
-        self.__last_alpha = 0
-        self.__min_alpha = self.__current_alpha
+        self.__min_alpha = 0
         self.__max_alpha = 255
-        self._set_animation_total_frames((self.__max_alpha - self.__min_alpha) * 4)
+        self.__current_alpha = self.__min_alpha
+        self.__last_alpha = 0
         self.__fade_in = True
         self.__icon_surface = super().render(self._icon, self._color)
-        super()._create_temporal_surface(self.__icon_surface.get_size())
+        self._set_animation_total_frames((self.__max_alpha - self.__min_alpha) * 4)
 
-    def _animate(self) -> bool:
+    def render(self) -> pygame.Surface:
+        self._set_animation_duration()
         if self.__fade_in:
             if self.__current_alpha < self.__max_alpha:
                 self.__current_alpha += self._frame_skip
@@ -28,11 +28,9 @@ class FontAwesomeIconFadeEffect(FontAwesomeIconBaseEffect):
                 self.__current_alpha -= self._frame_skip
             else:
                 self.__fade_in = True
-
-        if (self.__last_alpha != int(self.__current_alpha)):
+        if self.__last_alpha != int(self.__current_alpha):
             self.__icon_surface.set_alpha(int(self.__current_alpha))
-            self._blit(self.__icon_surface, (0, 0))
             self.__last_alpha = int(self.__current_alpha)
-            return True
+            return self.__icon_surface
         else:
-            return False
+            return None
