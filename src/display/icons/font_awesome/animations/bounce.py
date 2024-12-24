17bbae12c0361639c525dcb327e118f619d1821c
alex <766f6964+github@gmail.com>
2024-12-24 11:34:44 +0100

    rewrited bounce effect

--- src/display/icons/font_awesome/animations/bounce.py
+++ src/display/icons/font_awesome/animations/bounce.py
@@ -10,56 +10,43 @@ class FontAwesomeIconBounceEffect(FontAwesomeIconBaseEffect):
         self._animation_type = FontAwesomeAnimationType.BOUNCE
         self.__icon_surface = super().render(self._icon, self._color)
         total_height = self.__icon_surface.get_height() + (self.__icon_surface.get_height() // 2)
-        super()._create_temporal_surface((self.__icon_surface.get_width(), total_height))
-        self.__x = 0
-        self.__y = 0
+        self.__real_surface_size = (self.__icon_surface.get_width(), total_height)
         self.__min_y = 0
         self.__max_y = total_height - self.__icon_surface.get_height()
+        self.__distance = self.__max_y
+        self.__current_y = 0
+        self.__last_y = 0
         self.__falling = True
-        # TODO
-        diff = self.__max_y - self.__min_y
-        botes = 4
-        #print (f"Diff: {diff}")
-        #print (f"Botes: {botes}")
-        total = self.__calc_distance(diff, diff / botes, speed)
-        #print(f"Total {total}")
-        self._set_animation_total_frames(total)
-        self.__animation_unit = self._frame_skip
+        self._set_animation_total_frames(self.__distance * 8)
 
-    def __calc_distance(self, h_inicial, d, speed: FontAwesomeAnimationSpeed):
-        total_distancia = 0
-        altura_actual = h_inicial
-
-        while altura_actual > 0:
-            # Agrega subida y caída
-            total_distancia += 2 * altura_actual
-            # Reduce la altura para el próximo rebote
-            if (speed ==  FontAwesomeAnimationSpeed.FAST):
-                altura_actual -= d * 0.5
-            elif (speed ==  FontAwesomeAnimationSpeed.MEDIUM):
-                altura_actual -= d * 2
-            elif (speed ==  FontAwesomeAnimationSpeed.SLOW):
-                altura_actual -= d * 3
-
-        # Resta el último "bote" que no sube
-        total_distancia -= altura_actual
-        return total_distancia
-
-    def _animate(self) -> bool:
+    def __animate(self) -> None:
+        self._set_animation_duration()
         if self.__min_y < self.__max_y:
             if self.__falling:
-                if self.__y < self.__max_y:
-                    self.__y += self._frame_skip
+                if self.__current_y < self.__max_y:
+                    self.__current_y += self._frame_skip
                 else:
                     self.__falling = False
                     self.__min_y += self._frame_skip
-                    #self.__animation_unit += 1
             else:
-                if self.__y > self.__min_y:
-                    self.__y -= self._frame_skip
+                if self.__current_y > self.__min_y:
+                    self.__current_y -= self._frame_skip
                 else:
                     self.__falling = True
-            self._blit(self.__icon_surface, (self.__x, self.__y))
-            return True
+
+    @property
+    def __changed(self) -> bool:
+        return self.__last_y != int(self.__current_y)
+
+    def __update_changed_values(self) -> None:
+        self.__last_y = int(self.__current_y)
+
+    def render(self) -> pygame.Surface:
+        self.__animate()
+        if self.__changed:
+            tmp_surface = pygame.Surface(self.__real_surface_size, pygame.SRCALPHA)
+            tmp_surface.blit(self.__icon_surface, (0, self.__current_y))
+            self.__update_changed_values()
+            return tmp_surface
         else:
-            return False
+            return None
