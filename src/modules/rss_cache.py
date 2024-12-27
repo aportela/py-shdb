b42d13684ffb78b56b3eab821a2a20bc5f8814b4
alex <766f6964+github@gmail.com>
2024-12-27 01:38:46 +0100

    new rss cache class

---
+++ src/modules/rss_cache.py
@@ -0,0 +1,23 @@
+import os
+import hashlib
+
+from .module_cache import ModuleCache
+from ..utils.logger import Logger
+from .rss.rss_feed import RSSFeed
+
+DEFAULT_EXPIRATION_TIME=300 # 5 min
+
+class RSSCache(ModuleCache):
+    def __init__(self, logger: Logger, base_path: str, url: str) -> None:
+        super().__init__(logger=logger, base_path=os.path.join(base_path, "feeds"), filename=f"{hashlib.sha256(url.encode('utf-8')).hexdigest()[:64]}.rss", expiration=DEFAULT_EXPIRATION_TIME)
+        self.__url = url
+        super().check()
+
+    def _refresh(self) -> None:
+        rss = RSSFeed(self._log, self.__url)
+        try:
+            content = rss.get()
+            if not self.save_bytes(content):
+                raise ValueError(f"Error saving rss cache from URL: {self.__url} on path: {self.__fullpath}.")
+        except Exception as e:
+            raise ValueError(f"Error fetching rss from URL: {self.__url}")
