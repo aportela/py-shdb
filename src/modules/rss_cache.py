0865ab219a0631141f5d0782f06add3baa6246ce
alex <766f6964+github@gmail.com>
2024-12-27 12:55:18 +0100

    code cleanups

--- src/modules/rss_cache.py
+++ src/modules/rss_cache.py
@@ -1,22 +1,29 @@
 import os
 import hashlib
 
-from .module_cache import ModuleCache
+from .module_cache import ModuleCache, CacheError
 from ..utils.logger import Logger
 from .rss.rss_feed import RSSFeed
+
+class RSSCacheError(CacheError):
+    """Custom exception for RemoteImageCache-related errors."""
+    pass
 
 DEFAULT_EXPIRATION_TIME=300 # 5 min
 
 class RSSCache(ModuleCache):
     def __init__(self, logger: Logger, base_path: str, url: str) -> None:
         super().__init__(logger=logger, base_path=os.path.join(base_path, "feeds"), filename=f"{hashlib.sha256(url.encode('utf-8')).hexdigest()[:64]}.rss", expiration=DEFAULT_EXPIRATION_TIME)
+        if not url.startswith(('http://', 'https://')):
+            raise ValueError(f"Invalid URL format: {self.__url}")
         self.__url = url
         super()._check()
 
     def _refresh(self) -> None:
-        rss = RSSFeed(self._log, self.__url)
         try:
-            if not self.save(rss.get(self.__url)):
-                raise ValueError(f"Error saving rss cache from URL: {self.__url} on path: {self.__fullpath}.")
+            rss = RSSFeed(self._log, self.__url)
+            rss_data = rss.get(self.__url)
+            if not self.save(rss_data):
+                raise RSSCacheError(f"Error saving cache of rss from {self.__url}")
         except Exception as e:
-            raise ValueError(f"Error fetching rss from URL: {self.__url}")
+            raise RSSCacheError(f"Unexpected error while refreshing cache: {e}")
