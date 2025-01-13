987b265f137eb5aa7690ce99c429ed31bb43e641
alex <766f6964+github@gmail.com>
2025-01-13 10:42:26 +0100

    added topic callbacks

--- src/modules/mqtt/mqtt_client.py
+++ src/modules/mqtt/mqtt_client.py
@@ -1,13 +1,26 @@
-from typing import Optional
-import time
+from typing import Optional, Callable, Dict, List
 import paho.mqtt.client as mqtt
-
+import time
 from ...utils.logger import Logger
-class MQTTClient():
 
-    def __init__(self, broker: str, port: int = 1883, username: Optional[str] = None, password: Optional[str] = None, topic: Optional[str] = None) -> None:
+class MQTTClient:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        """
+        Singleton: ensures unique instance of MQTTClient
+        """
+        if cls._instance is None:
+            cls._instance = super(MQTTClient, cls).__new__(cls)
+        return cls._instance
+
+    def __init__(self, broker: str, port: int = 1883, username: Optional[str] = None,
+                 password: Optional[str] = None) -> None:
+        if hasattr(self, "_initialized"):
+            return
+        self._initialized = True
+
         self.__log = Logger()
-
         self.__client = mqtt.Client()
 
         self.__client.on_connect = self.__on_connect
@@ -15,52 +28,65 @@ class MQTTClient():
         self.__client.on_message = self.__on_message
 
         if username:
-            print(f"Usuario: {username}")
-            print(f"Password: {password}")
             self.__client.username_pw_set(username, password)
 
-        if not topic:
-            raise ValueError("Topic not set")
-
-        self.__topic = topic
-        #self.__client.on_log = self.__on_log
-
-        if not broker:
-            raise ValueError("Broker not set")
+        self.__callbacks: Dict[str, List[Callable[[str, str], None]]] = {}
 
         self.__client.connect(broker, port, 60)
         self.__client.loop_start()
 
-    def __on_log(self, client, userdata, level, buf):
-        self.__log.debug(f"Log: {buf}")
-
     def __on_connect(self, client, userdata, flags, rc):
-        if self.__topic is not None:
-            self.__log.info(f"Connected, subscribe to topic {self.__topic}")
-            result, _ = client.subscribe(self.__topic)
-            if result != mqtt.MQTT_ERR_SUCCESS:
-                self.__log.error(f"Failed to subscribe to topic: {self.__topic}")
+        """
+        MQTT broker on connect event
+        """
+        if rc == 0:
+            self.__log.info(f"Connected to broker.")
         else:
-            self.__log.info(f"Connected")
+            self.__log.warning(f"Error while connecting to broker. ErrorCode: {rc}")
+
+    def __on_disconnect(self, client, userdata, rc):
+        """
+        MQTT broker on disconnect event
+        """
+        self.__log.warning("Broker disconnected.")
+        if rc != 0:  # re-connect if disconection was not intentional
+            self.__log.info("Trying re-connect...")
+            self.__reconnect()
 
     def __reconnect(self):
+        """
+        re-connect to MQTT broker if disconnected
+        """
         while True:
             try:
                 self.__client.reconnect()
-                self.__log.info(f"Reconnect successfull")
+                self.__log.info("Re-connect successful.")
                 break
             except Exception as e:
-                self.__log.warning(f"Error while reconnecting: {e}")
+                self.__log.warning(f"Error while re-connecting: {e}")
                 time.sleep(5)
 
-    def __on_disconnect(self, client, userdata, rc):
-        if rc != 0:  # reconnect if disconnection was not forced
-            if rc == mqtt.MQTT_ERR_CONN_REFUSED:
-                raise ValueError(f"Broker disconnected, connection refused")
-            else:
-                self.__log.info(f"Broker disconnected, auto-reconnecting again")
-                self.__reconnect()
-
     def __on_message(self, client, userdata, msg):
-        print("Message received")
-        print(f"{msg.topic}: {msg.payload.decode()}")
+        """
+        MQTT broker on message event
+        Calls registered callbacks for corresponding topic
+        """
+        payload = msg.payload.decode()
+        #self.__log.info(f"Received message on {msg.topic}: {payload}")
+        for topic, callbacks in self.__callbacks.items():
+            if mqtt.topic_matches_sub(topic, msg.topic):
+                for callback in callbacks:
+                    callback(msg.topic, payload)
+
+    def add_callback(self, topic: str, callback: Callable[[str, str], None]):
+        """
+        Registers callback for specific topic
+        :param topic: associated topic
+        :param callback: function to execute if message is received on topic
+        """
+        if topic not in self.__callbacks:
+            self.__callbacks[topic] = []
+            self.__log.info(f"Subscribe to the topic {topic}.")
+            self.__client.subscribe(topic)
+        self.__callbacks[topic].append(callback)
+        self.__log.info(f"Callback registered for topic: {topic}")
