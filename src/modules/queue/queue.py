a88ec46a57649466d890f62cff2efeafc1a75b9d
alex <766f6964+github@gmail.com>
2025-01-14 11:44:22 +0100

    added constructor params on QueueMSG, debug fixed

--- src/modules/queue/queue.py
+++ src/modules/queue/queue.py
@@ -1,12 +1,15 @@
-from typing import Optional
-#import time
+from typing import Optional, Any
 from ...utils.logger import Logger
-
+import time
 import queue
 
 class QueueMSG:
-    def __init__(self):
-        self.__value = None
+    def __init__(self, value: Any, timestamp: Optional[int] = None):
+        self.value = value
+        if timestamp is not None:
+            self.timestamp = timestamp
+        else:
+            self.timestamp = time.time()
 
 class Queue:
     def __init__(self):
@@ -14,13 +17,13 @@ class Queue:
         self.__shared_queue = queue.Queue()
 
     def enqueue(self, msg: QueueMSG):
-        self.__log.debug("enqueue message: {msg}")
+        self.__log.debug(f"enqueue message: {msg}")
         self.__shared_queue.put(msg)
 
     def dequeue(self) -> Optional[QueueMSG]:
         try:
             msg = self.__shared_queue.get_nowait()
-            self.__log.debug("dequeue message: {msg}")
+            self.__log.debug(f"dequeue message: {msg}")
             return msg
         except queue.Empty:
             self.__log.debug("empty queue")
