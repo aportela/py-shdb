5645928a1b10f43b4fa42a78ad6bb8b5252d77c2
alex <766f6964+github@gmail.com>
2025-01-18 10:30:37 +0100

    fixed alpha background, restored graph, added min/max y axis limits

--- src/display/widgets/charts/line_chart_widget.py
+++ src/display/widgets/charts/line_chart_widget.py
@@ -1,7 +1,7 @@
 import pygame
 import random
 
-from typing import Optional
+from typing import Optional, Any
 from .chart_widget import ChartWidget, ChartWidgetHorizontalTextBlock
 from ..widget import DEFAULT_WIDGET_BORDER_COLOR
 from ....modules.mqtt.data_sources.mqtt_data_source import MQTTDataSource
@@ -9,24 +9,31 @@ from ....modules.queue.queue import QueueMSG
 
 class LineChartWidget(ChartWidget):
 
-    def __init__(self, parent_surface: pygame.Surface, name: str, rect: pygame.Rect, background_color: tuple[int, int, int] = None, border: bool = False, border_color: tuple[int, int, int] = DEFAULT_WIDGET_BORDER_COLOR, top_title_block: Optional[ChartWidgetHorizontalTextBlock] = None, bottom_legend_block: Optional[ChartWidgetHorizontalTextBlock] = None, data_source: MQTTDataSource = None) -> None:
+    def __init__(self, parent_surface: pygame.Surface, name: str, rect: pygame.Rect, background_color: tuple[int, int, int] = None, border: bool = False, border_color: tuple[int, int, int] = DEFAULT_WIDGET_BORDER_COLOR, top_title_block: Optional[ChartWidgetHorizontalTextBlock] = None, bottom_legend_block: Optional[ChartWidgetHorizontalTextBlock] = None, data_source: MQTTDataSource = None, y_axis_min_value: Any = 0, y_axis_max_value: Any = 0) -> None:
         super().__init__(parent_surface = parent_surface, name = name, rect = rect, background_color = background_color, border = border, border_color = border_color, top_title_block = top_title_block, bottom_legend_block = bottom_legend_block)
         self._refresh_required = True
         self.__data_source = data_source
+        self._y_axis_min_value = y_axis_min_value
+        self._y_axis_max_value = y_axis_max_value
         self.__min_value = None
         self.__max_value = None
         self.__current_value = None
-        self.__tmp_surface = pygame.Surface((self.width, self.height))
+        self.__tmp_surface = pygame.Surface((self.width, self.height), pygame.SRCALPHA)
         self.__tmp_surface.fill((0, 0, 0))
         self._chart_height = self.height
         self.__top_title_surface = None
         self.__refresh_top_title_surface()
+        self.__graph_surface_y_offset = 0
         if self.__top_title_surface is not None:
             self._chart_height -= self.__top_title_surface.get_height()
+            self.__graph_surface_y_offset += self.__top_title_surface.get_height()
         self.__bottom_legend_surface = None
         self.__refresh_bottom_legend_surface()
         if self.__bottom_legend_surface is not None:
             self._chart_height -= self.__top_title_surface.get_height()
+
+        self.__graph_surface = pygame.Surface((self.width, self._chart_height), pygame.SRCALPHA)
+
 
     def set_data_source(self, data_source: MQTTDataSource):
         self.__data_source = data_source
@@ -75,29 +82,27 @@ class LineChartWidget(ChartWidget):
                 )
 
     def __render_graph(self, value: int) -> pygame.Surface:
-        surface = pygame.Surface((self.width, self.height))
-        surface.fill((0, 0, 0))
+        surface = pygame.Surface((self.width, self._chart_height), pygame.SRCALPHA)
 
-        if self._title_surface is not None:
-            surface.blit(self._title_surface, (0,0))
+        #surface.fill((0, 0, 0))
 
-        start_pos = (4, 4)
-        if self._title_surface is not None:
-            start_pos = (4, 4 + self._title_surface.get_height() + 4)
-        end_pos = (4, self.height - 4)
-        pygame.draw.line(surface=surface, color=(255, 255, 255), start_pos=start_pos, end_pos=end_pos, width=2)
+        """
+        start_pos = (0, 0) #(4, 4)
+        end_pos = (0, self._chart_height)
+        pygame.draw.line(surface = surface, color=(255, 255, 255), start_pos = start_pos, end_pos = end_pos, width = 2)
 
-        start_pos = (4, self.height - 4)
-        end_pos = (self.width - 4, self.height - 4)
-        pygame.draw.line(surface=surface, color=(255, 255, 255), start_pos=start_pos, end_pos=end_pos, width=2)
+        start_pos = (self.width, 0)
+        end_pos = (self.width, self._chart_height)
+        pygame.draw.line(surface = surface, color=(255, 255, 255), start_pos = start_pos, end_pos = end_pos, width = 2)
 
+        """
         x = self.__graph_surface.get_width() -1
         y = self.__graph_surface.get_height() -1
 
-        v = int(self.__map_value(value, 0, y, 0, 100))
+        v = int(self.__map_value(value, 0, y, self._y_axis_min_value, self._y_axis_max_value))
+        #print(f"Value: {value} - Mapped value: {v} - {0} a {y}, {self._y_axis_min_value} a {self._y_axis_max_value}")
 
-
-        pygame.draw.line(surface=self.__graph_surface, color=(0, 0, 0), start_pos=(x, 0), end_pos=(x, y), width=1) # delete with black vertical line
+        pygame.draw.line(surface = self.__graph_surface, color=(0, 0, 0, 0), start_pos=(x, 0), end_pos=(x, y), width=1) # delete with black vertical line
         self.__graph_surface.set_at((x, y - v), (255, 0, 255)) # pixel
         pygame.draw.line(surface=self.__graph_surface, color=(125, 0, 125), start_pos=(x, y - v +1), end_pos=(x, y), width=1) ## line (fill bg)
         surface.blit(source = self.__graph_surface, dest=(6, 6))
@@ -117,8 +122,7 @@ class LineChartWidget(ChartWidget):
                     self.__refresh_bottom_legend_surface()
                 super()._blit(self.__bottom_legend_surface, (0, self.height - self.__bottom_legend_surface.get_height()))
 
-                #self.__tmp_surface.blit(self.__top_title_surface, (0, 0))
-            #super()._blit(self.__render_graph(int(value.value)), (0, 0))
+            super()._blit(self.__render_graph(int(self.__current_value)), (0, self.__graph_surface_y_offset))
 
             super()._render()
             #self._refresh_required = False
